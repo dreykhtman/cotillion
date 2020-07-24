cddc6964d88bc9051ad827d7251f3946f7c7221e
Dima <myuziw8s1t@gmail.com>
2020-07-23 20:54:14 -0400

    experiment with sliding animation

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-// // Intersection Observer
-// const options = {
-//   root: null,
-//   rootMargin: '-15%',
-//   threshold: 1.0,
-// };
+const fiftyPercent = document.getElementsByClassName('heading-2');
 
-// // const description = document.querySelector('.description');
-// // console.log(description);
+// Intersection Observer
+const options = {
+  root: null,
+  rootMargin: '5%',
+  threshold: 0.1,
+};
 
-// const callback = (entries, observer) => {
-//   // const visibility = entries[0].intersectionRatio;
-//   // if (visibility >= 1) {
-//   // }
-// };
+// const description = document.querySelector('.description');
+// console.log(description);
 
-// const observer = new IntersectionObserver(callback, options);
-// const target = document.getElementById('target');
-// observer.observe(target);
+const callback = (entries, observer) => {
+  const visibility = entries[0].intersectionRatio;
+  if (visibility >= 0.1) {
+    fiftyPercent[1].style.transform = 'translateX(20vw)';
+    fiftyPercent[2].style.transform = 'translateX(-20vw)';
+  }
+};
+
+const observer = new IntersectionObserver(callback, options);
+const target = document.getElementById('target');
+observer.observe(target);
