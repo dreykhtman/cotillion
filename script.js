0a1b8b3a2906926aec6be0121c59318c31bbeb51
Dima <myuziw8s1t@gmail.com>
2020-07-24 15:42:05 -0400

    Update description list

--- script.js
+++ script.js
@@ -12,10 +12,18 @@ const options = {
 
 const callback = (entries, observer) => {
   const visibility = entries[0].intersectionRatio;
-  if (visibility >= 0.1) {
-    fiftyPercent[1].style.transform = 'translateX(20vw)';
-    fiftyPercent[2].style.transform = 'translateX(-20vw)';
-  }
+  // if (visibility >= 0.1) {
+  //   fiftyPercent[1].style.transform = 'translateX(20vw)';
+  //   fiftyPercent[2].style.transform = 'translateX(-20vw)';
+
+  //   fiftyPercent[1].innerHTML = '100%';
+  //   fiftyPercent[2].innerHTML = '';
+  // } else {
+  //   fiftyPercent[1].style.transform = 'translateX(0)';
+  //   fiftyPercent[2].style.transform = 'translateX(0)';
+  //   fiftyPercent[1].innerHTML = '50%';
+  //   fiftyPercent[2].innerHTML = '50%';
+  // }
 };
 
 const observer = new IntersectionObserver(callback, options);
