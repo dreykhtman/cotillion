c030c708148f96d6b75fdb1594d9752cde7cfe9e
Dima <myuziw8s1t@gmail.com>
2020-08-25 20:19:06 -0400

    Make navbar circles clickable

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 //
-//
 // Observer for the 50% element slide animation
-//
 //
 
 const targetList = document.getElementById('target-list');
@@ -17,10 +15,14 @@ const options = {
 
 const callback = (entries, observer) => {
   entries.forEach((entry) => {
-    if (entry.boundingClientRect.y < 0) {
-      fiftyLeft.classList.toggle('slide-right');
-      fiftyRight.classList.toggle('slide-left');
-      hundred.classList.toggle('slide-down');
+    if (entry.boundingClientRect.y < 0 && !entry.isIntersecting) {
+      fiftyLeft.classList.add('slide-right');
+      fiftyRight.classList.add('slide-left');
+      hundred.classList.add('slide-down');
+    } else {
+      fiftyLeft.classList.remove('slide-right');
+      fiftyRight.classList.remove('slide-left');
+      hundred.classList.remove('slide-down');
     }
   });
 };
@@ -29,79 +31,9 @@ const observer = new IntersectionObserver(callback, options);
 observer.observe(targetList);
 
 //
-//
-// Jumping logo observer
-//
+// Navbar & jumping logo observer
 //
 
-/*
-const jumpingLogo = document.getElementById('jumping-logo');
-const albums = document.querySelectorAll('.album');
-const ourAlbums = document.getElementById('our-albums');
-
-const artOptions = {
-  root: null,
-  rootMargin: '0px 0px -57% 0px',
-  threshold: 0,
-};
-
-const coverStack = [];
-
-const classToggler = (id) => {
-  const togglePrevious = () => {
-    const selectedElement = document.getElementById(
-      coverStack[coverStack.length - 1]
-    );
-
-    if (selectedElement) {
-      selectedElement.classList.toggle('animation-fadeout');
-      selectedElement.classList.toggle('animation-fadein');
-    } else {
-      jumpingLogo.classList.toggle('animation-fadeout');
-      jumpingLogo.classList.toggle('animation-fadein');
-    }
-  };
-
-  if (coverStack[coverStack.length - 1] !== id) {
-    togglePrevious();
-    coverStack.push(id);
-    togglePrevious();
-  } else {
-    togglePrevious();
-    coverStack.pop();
-    togglePrevious();
-  }
-};
-
-const artCallback = (entries, observer) => {
-  entries.forEach((entry) => {
-    if (
-      entry.boundingClientRect.y <= window.scrollY &&
-      entry.boundingClientRect.y > 0 &&
-      entry.target.id === 'our-albums'
-    ) {
-      ourAlbums.classList.toggle('change-color');
-    } else if (
-      entry.boundingClientRect.y <= window.scrollY &&
-      entry.boundingClientRect.y > 0
-    ) {
-      classToggler(entry.target.children[0].id);
-    }
-  });
-};
-
-const artObserver = new IntersectionObserver(artCallback, artOptions);
-albums.forEach((cover) => artObserver.observe(cover));
-artObserver.observe(ourAlbums);
-*/
-
-//
-//
-// Navbar observer
-//
-//
-
-// const navSelected = document.getElementsByClassName('nav-selected');
 const sections = document.querySelectorAll('.section');
 const ourAlbums = document.getElementById('our-albums');
 
@@ -156,17 +88,10 @@ const navbarCallback = (entries, observer) => {
         jumpingLogo.classList.remove('animation-fadein');
       }
     }
-
-    // if (entry.isIntersecting && currentAlbumCoverLogo) {
-    //   currentAlbumCoverLogo.classList.remove('animation-fadeout');
-    //   currentAlbumCoverLogo.classList.add('animation-fadein');
-    // } else if (currentAlbumCoverLogo && !entry.isIntersecting) {
-    //   currentAlbumCoverLogo.classList.add('animation-fadeout');
-    //   currentAlbumCoverLogo.classList.remove('animation-fadein');
-    // }
   });
 };
 
 const navbarObserver = new IntersectionObserver(navbarCallback, navbarOptions);
+
 sections.forEach((section) => navbarObserver.observe(section));
 navbarObserver.observe(ourAlbums);
